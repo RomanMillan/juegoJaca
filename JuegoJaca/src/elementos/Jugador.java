20d47fa9de05a8a73407435a754ed6f52a6d296d
roman millan <romandos962m@gmail.com>
2022-05-09 13:59:57 +0200

    avance

--- JuegoJaca/src/elementos/Jugador.java
+++ JuegoJaca/src/elementos/Jugador.java
@@ -70,10 +70,12 @@ public class Jugador {
 	}
 	
 	public int getMagiaParaLuchar() {
-		ThreadLocalRandom fuerzaR = ThreadLocalRandom.current();
-        return fuerzaR.nextInt(0, getFuerza());
+		ThreadLocalRandom magia = ThreadLocalRandom.current();
+        return magia.nextInt(0, getMagia());
 	}
 
+	//toString
+	
 	public String resumen() {
 		return "Jugador [dinero=" + dinero + ", gemas=" + gemas + ", player=" + player + ", pociones=" + pociones + "]";
 	}
@@ -91,18 +93,57 @@ public class Jugador {
 			}else if(enemigo.getDinero()>0) {
 				resultado = Constantes.GANA_DINERO;
 			}else {
-				resultado = Constantes.PIERDE_MUERE;
+				resultado = Constantes.GANA_MUERE;
 			}		
 		}else {
-			
+			if(this.getPociones()>0) {
+				resultado = Constantes.PIERDE_USA_POCIMA;
+			}else if(this.getDinero()>0) {
+				resultado = Constantes.PIERDE_DINERO;
+			}else {
+				resultado = Constantes.PIERDE_MUERE;
+			}
+		}
+		return resultado;
+	}
+	
+	public int getVelocidadParaLuchar() {
+		ThreadLocalRandom velocidad = ThreadLocalRandom.current();
+        return velocidad.nextInt(0, getVelocidad());
+	}
+	
+	public void encuentraDinero() {
+		setDinero(getDinero()+1);
+	}
+	
+	public void encuentraPocion() {
+		setPociones(getPociones()+1);
+	}
+	
+	public void encuentraGema() {
+		setGemas(getGemas()+1);
+	}
+	
+	public int encuentraRoca() {
+		//TODO Exception jugador?
+		int resultado;
+		if(getGemas()>0) {
+			resultado = Constantes.ROMPE_ROCA_CON_GEMA;
+			setGemas(getGemas()-1);
+		}else if(getMagia()>4) {
+			resultado = Constantes.PIERDE_A_LA_ROCA;
+		}else {
+			resultado = Constantes.GANA_A_LA_ROCA;
 		}
 		
 		return resultado;
-		
 	}
 	
 	
-	
-	
-	
 }
+
+
+
+
+
+
--- JuegoJaca/src/main/principalAux.java
+++ JuegoJaca/src/main/principalAux.java
@@ -5,7 +5,7 @@ import elementos.Coordenada;
 public class principalAux {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		
 //		Coordenada coord = new Coordenada(1,2);
 //		System.out.println(coord);
