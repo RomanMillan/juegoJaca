aceee8a4506288f92b91f57d7b9f3e680cf4cec8
roman millan <romandos962m@gmail.com>
2022-05-20 18:17:50 +0200

    jUnit

--- JuegoJaca/src/test/JuegoTest.java
+++ JuegoJaca/src/test/JuegoTest.java
@@ -1,20 +1,27 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
+import elementos.JugadorException;
 import elementos.PlayerType;
 import logicaJuego.Juego;
+import logicaJuego.JuegoException;
 
 class JuegoTest {
 
 	@Test
-	void MoverJugadorTest() {
-		PlayerType[] = 
-		Juego j = new Juego(PlayerType[]);
+	void MoverJugadorTest() throws JuegoException, JugadorException {
+		ArrayList<PlayerType> jugadores = new ArrayList<>();
+		jugadores.add(PlayerType.ELFO);
+		PlayerType[] ordenJugadores = new PlayerType[1];
+		Juego j = new Juego(ordenJugadores);
+		j.movePlayer('N');
 		
-		j.movePlayer('F');
 	}
 
 }
